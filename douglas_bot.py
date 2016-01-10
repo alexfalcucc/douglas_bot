f12f8d80722b5f2a1cd79a43f8accf365df3a698
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-10 07:00:32 -0200

    added date status

--- douglas_bot.py
+++ douglas_bot.py
@@ -1,8 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 import sys
 import time
 import random
 import datetime
 import telepot
+import calendar
+
+morning_words = ['@doguinha_bot morning', '@doguinha_bot good morning']
+
+months = {
+    'January': 'Janeiro',
+    'February': 'Fevereiro',
+    'March': 'Março',
+    'April': 'Abril',
+    'May': 'Maio',
+    'June': 'Junho',
+    'July': 'Julho',
+    'August': 'Agosto',
+    'September': 'Setembro',
+    'October': 'Outubro',
+    'November': 'Novembro',
+    'December': 'Dezembro'
+}
 
 
 def handle(msg):
@@ -13,10 +33,12 @@ def handle(msg):
 
     print 'Got command: %s' % command
 
-    if command == '@doguinha_bot morning':
+    if command.lower() in morning_words:
         bot.sendMessage(chat_id, "Good morning, {}!".format(name))
-    elif command == '/time':
-        bot.sendMessage(chat_id, str(datetime.datetime.now()))
+    elif command == '@doguinha_bot data?':
+        day = datetime.datetime.now().day
+        month = months.get(calendar.month_name[datetime.datetime.now().month], '')
+        bot.sendMessage(chat_id, u"É dia de você calar essa boca. \n\nBrincadeira, hoje é dia {day} de {month} \U0001f605".format(day=day, month=month))
 
 bot = telepot.Bot('142375463:AAFf1mMbT1O3rxOCaQ8j0hzdU_Hc5Wh4kj0')
 bot.notifyOnMessage(handle)
