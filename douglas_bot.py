0c6217e1b2ecacda9277d1210e0c554ef4bbcebc
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-10 18:13:59 -0200

    added time reply

--- douglas_bot.py
+++ douglas_bot.py
@@ -28,9 +28,13 @@ welcome_count = 0
 
 
 def handle(msg):
+    command = ''
     print msg
-    chat_id = msg['chat']['id']
-    command = msg['text']
+    content_type, chat_type, chat_id = telepot.glance2(msg)
+    print content_type, chat_type, chat_id
+    if content_type == 'text':
+        command = msg['text']
+
     name = msg['from']['first_name']
 
     print 'Got command: %s' % command
@@ -53,6 +57,13 @@ def handle(msg):
         print globals()['welcome_count']
     elif command.lower() == '@doguinha_bot eu te amo!':
         bot.sendMessage(chat_id, u"Eu tambem amo vc, {} \u2764\ufe0f".format(name))
+    elif command.lower() == u'@doguinha_bot que horas s\xe3o?':
+        msg = u"É muita hipocrisia da sua parte me perguntar isso, {}... "\
+              u"Você pode vizualisar facilmente as horas olhando para parte "\
+              u"inferior direita do seu comentário."
+        bot.sendMessage(chat_id, msg.format(name))
+        bot.sendChatAction(chat_id, 'upload_document')
+        bot.sendDocument(chat_id, "BQADAQADEwADnqxzCGp0fqkzsPC6Ag")
 
 bot = telepot.Bot('142375463:AAFf1mMbT1O3rxOCaQ8j0hzdU_Hc5Wh4kj0')
 bot.notifyOnMessage(handle)
