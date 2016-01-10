28f4190b87a0bb19970f2aae91e2857120d4cd0e
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-10 07:27:01 -0200

    update globals var

--- douglas_bot.py
+++ douglas_bot.py
@@ -24,6 +24,8 @@ months = {
     'December': 'Dezembro'
 }
 
+welcome_count = 0
+
 
 def handle(msg):
     print msg
@@ -35,10 +37,20 @@ def handle(msg):
 
     if command.lower() in morning_words:
         bot.sendMessage(chat_id, "Good morning, {}!".format(name))
-    elif command == '@doguinha_bot data?':
+    elif command == '@doguinha_bot data':
         day = datetime.datetime.now().day
         month = months.get(calendar.month_name[datetime.datetime.now().month], '')
         bot.sendMessage(chat_id, u"É dia de você calar essa boca. \n\nBrincadeira, hoje é dia {day} de {month} \U0001f605".format(day=day, month=month))
+    elif command == '@doguinha_bot bem vindo!':
+        if welcome_count < 1:
+            msg_wel = u"Eu sempre estive aqui, idiota! \U0001f610"
+        elif welcome_count == 1:
+            msg_wel = u"Não repito. \U0001f48b"
+        else:
+            msg_wel = u"\U0001f610"
+        bot.sendMessage(chat_id, msg_wel)
+        globals()['welcome_count'] += 1
+        print globals()['welcome_count']
 
 bot = telepot.Bot('142375463:AAFf1mMbT1O3rxOCaQ8j0hzdU_Hc5Wh4kj0')
 bot.notifyOnMessage(handle)
