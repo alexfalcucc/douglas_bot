0058f6ae272a967dd875b1f28682de123cf5407e
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-13 16:15:43 -0200

    added pickledb library

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
 
 requires = [
     'schedule',
     'telepot',
+    'pickledb',
     ]
 
 setup(name='douglas_bot',
