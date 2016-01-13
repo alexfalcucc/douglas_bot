16c6f71d261778facd133bd4561005c27c6febae
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-13 01:58:20 -0200

    added ed governament bot

---
+++ ed.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Robô Ed Telegram Bot
+# This program is dedicated to the public domain under the CC0 license.
+import urllib3
+
+
+def ed(text):
+    http = urllib3.PoolManager()
+    url = 'http://www.ed.conpet.gov.br/mod_perl/bot_gateway.cgi?server=0.0.0.0%3A8085&charset_post=utf-8&charset=utf-8&pure=1&js=0&tst=1&msg=' + text
+    r = http.request('GET', url)
+    return r.data.strip()
