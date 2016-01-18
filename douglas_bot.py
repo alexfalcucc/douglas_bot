c55beba59ea552ef44c1f7493f9a9276b8f2d870
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-18 15:17:32 -0200

    added logging and send email if any error happens

--- douglas_bot.py
+++ douglas_bot.py
@@ -14,6 +14,8 @@ import telepot
 import calendar
 import schedule
 import pickledb
+import logging
+import logging.handlers
 from external.ed import get_ed_reply, count_ed_mgs
 from external.simsimi import get_simsimi_reply, count_simsimi_msg
 from utils.utils import utf8_encode, remove_bot_name, verify_text
@@ -30,6 +32,50 @@ __maintainer__ = "Alexsander Falcucci"
 __license__ = "MIT"
 
 
+class TlsSMTPHandler(logging.handlers.SMTPHandler):
+    def emit(self, record):
+        """
+        Emit a record.
+ 
+        Format the record and send it to the specified addressees.
+        """
+        try:
+            import smtplib
+            import string # for tls add this line
+            try:
+                from email.utils import formatdate
+            except ImportError:
+                formatdate = self.date_time
+            port = self.mailport
+            if not port:
+                port = smtplib.SMTP_PORT
+            smtp = smtplib.SMTP(self.mailhost, port)
+            msg = self.format(record)
+            msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\nDate: %s\r\n\r\n%s" % (
+                            self.fromaddr,
+                            string.join(self.toaddrs, ","),
+                            self.getSubject(record),
+                            formatdate(), msg)
+            if self.username:
+                smtp.ehlo() # for tls add this line
+                smtp.starttls() # for tls add this line
+                smtp.ehlo() # for tls add this line
+                smtp.login(self.username, self.password)
+            smtp.sendmail(self.fromaddr, self.toaddrs, msg)
+            smtp.quit()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+logger = logging.getLogger()
+
+gm = TlsSMTPHandler(("smtp.gmail.com", 587), 'alex.falcucci@gmail.com', ['alex.falcucci@gmail.com'], 'Error found!', ('alex.falcucci@gmail.com', 'whdesign007'))
+gm.setLevel(logging.ERROR)
+
+logger.addHandler(gm)
+
+
 db = pickledb.load(os.environ['HOME'] + '/douglas_db/douglas.db', True)
 
 
@@ -39,170 +85,173 @@ def handle(msg):
     We have to refactor it puting all commands at a tuple or list, etc.
     The bot username will be dinamically alterable, too.
     """
-    global bot_names
-    command = ''
-    print msg
-    content_type, chat_type, chat_id = telepot.glance2(msg)
-    print content_type, chat_type, chat_id
-    if content_type == 'text':
-        command = utf8_encode(msg['text'].lower())
+    try:
+        global bot_names
+        command = ''
+        print msg
+        content_type, chat_type, chat_id = telepot.glance2(msg)
+        print content_type, chat_type, chat_id
+        if content_type == 'text':
+            command = utf8_encode(msg['text'].lower())
 
-    name = utf8_encode(msg['from']['first_name'])
+        name = utf8_encode(msg['from']['first_name'])
 
-    print 'Got command: %s' % command
+        print 'Got command: %s' % command
 
-    names_to_check = verify_text(bot_names, command)
-    print names_to_check
+        names_to_check = verify_text(bot_names, command)
+        print names_to_check
 
-    if names_to_check or chat_type == 'private':
-        command = remove_bot_name(names_to_check, command)
-        print command
+        if names_to_check or chat_type == 'private':
+            command = remove_bot_name(names_to_check, command)
+            print command
 
-        if command.lower() in morning_words:
-            bot.sendMessage(chat_id, "Good morning, {}!".format(name))
-        elif command.lower() in night_words:
-            bot.sendMessage(chat_id, "Good night, {}!".format(name))
-        elif command == 'que dia é hoje?':
-            day = datetime.datetime.now().day
-            month = months.get(calendar.month_name[datetime.datetime.now().month], '')
-            bot.sendMessage(
-                chat_id, "É dia de você calar essa boca. \n\nBrincadeira, hoje é dia {day} de {month} \U0001f605".format(day=day, month=month))
-        elif command == 'bem vindo!':
-            if welcome_count < 1:
-                msg_wel = "Eu sempre estive aqui, idiota! {}".format(Emoji.NEUTRAL_FACE)
-            elif welcome_count == 1:
-                msg_wel = "Não repito. {}".format(Emoji.NEUTRAL_FACE)
-            else:
-                msg_wel = "{}".format(Emoji.NEUTRAL_FACE)
-            bot.sendMessage(chat_id, msg_wel)
-            globals()['welcome_count'] += 1
-            print globals()['welcome_count']
-        elif command.lower() in love_words:
-            msgs = [
-                "Eu tambem amo vc, {}! {}{}".format(name,
-                                                    Emoji.BLACK_HEART_SUIT,
-                                                    Emoji.BLACK_HEART_SUIT),
-                "Legal.",
-            ]
-            msg = random.choice(msgs)
-            bot.sendMessage(chat_id, msg)
-            if msg == "Legal.":
+            if command.lower() in morning_words:
+                bot.sendMessage(chat_id, "Good morning, {}!".format(name))
+            elif command.lower() in night_words:
+                bot.sendMessage(chat_id, "Good night, {}!".format(name))
+            elif command == 'que dia é hoje?':
+                day = datetime.datetime.now().day
+                month = months.get(calendar.month_name[datetime.datetime.now().month], '')
+                bot.sendMessage(
+                    chat_id, "É dia de você calar essa boca. \n\nBrincadeira, hoje é dia {day} de {month} \U0001f605".format(day=day, month=month))
+            elif command == 'bem vindo!':
+                if welcome_count < 1:
+                    msg_wel = "Eu sempre estive aqui, idiota! {}".format(Emoji.NEUTRAL_FACE)
+                elif welcome_count == 1:
+                    msg_wel = "Não repito. {}".format(Emoji.NEUTRAL_FACE)
+                else:
+                    msg_wel = "{}".format(Emoji.NEUTRAL_FACE)
+                bot.sendMessage(chat_id, msg_wel)
+                globals()['welcome_count'] += 1
+                print globals()['welcome_count']
+            elif command.lower() in love_words:
+                msgs = [
+                    "Eu tambem amo vc, {}! {}{}".format(name,
+                                                        Emoji.BLACK_HEART_SUIT,
+                                                        Emoji.BLACK_HEART_SUIT),
+                    "Legal.",
+                ]
+                msg = random.choice(msgs)
+                bot.sendMessage(chat_id, msg)
+                if msg == "Legal.":
+                    bot.sendChatAction(chat_id, 'upload_document')
+                    bot.sendDocument(chat_id, "BQADBAADdwMAAgMdZAdPtWmOPGN1IQI")
+            elif command.lower() == 'que horas são?':
+                msg = "É muita hipocrisia da sua parte me perguntar isso {}... "\
+                      "Você pode vizualisar facilmente as horas olhando para parte "\
+                      "inferior direita do seu comentário."
+                bot.sendMessage(chat_id, msg.format(name))
                 bot.sendChatAction(chat_id, 'upload_document')
-                bot.sendDocument(chat_id, "BQADBAADdwMAAgMdZAdPtWmOPGN1IQI")
-        elif command.lower() == 'que horas são?':
-            msg = "É muita hipocrisia da sua parte me perguntar isso {}... "\
-                  "Você pode vizualisar facilmente as horas olhando para parte "\
-                  "inferior direita do seu comentário."
-            bot.sendMessage(chat_id, msg.format(name))
-            bot.sendChatAction(chat_id, 'upload_document')
-            bot.sendDocument(chat_id, "BQADAQADEwADnqxzCGp0fqkzsPC6Ag")
-        elif command.lower() == 'nós te amamos!':
-            msg = "Ah é?! Foda-se."
-            bot.sendMessage(chat_id, msg)
-            bot.sendChatAction(chat_id, 'upload_document')
-            bot.sendDocument(chat_id, "BQADBAADYwMAAiUcZAe1DjlP-IMGhQI")
-        elif command.lower() == 'é bininu binina ou binunu binino?'.lower():
-            msg = "bininu."
-            bot.sendMessage(chat_id, msg)
-        elif command.lower() == 'qual sua idade?':
-            msg = "Você sabe a idade de Deus, seu criador? Pois é, sou 1 ano mais novo que Ele."
-            bot.sendMessage(chat_id, msg)
-        elif command.lower() == '/emojis':
-            msg1, msg2, msg3, msg4, msg5 = get_all_emojis()
-            bot.sendMessage(chat_id, msg1)
-            bot.sendMessage(chat_id, msg2)
-            bot.sendMessage(chat_id, msg3)
-            bot.sendMessage(chat_id, msg4)
-            bot.sendMessage(chat_id, msg5)
-        elif command.lower() in fuck_words:
-            msg = [
-                "Querido, por favor! Tenha boas maneiras! Você tem que me convidar pra jantar primeiro.",
-                "Entre na fila.",
-                "Sonhando novamente, querido?",
-                "Se sentindo sozinho de novo, ha?",
-                "É só eu ou você diz isso para todos?",
-                "Sério? Agora?",
-                "Não obrigado. Eu passo.",
-            ]
-            bot.sendMessage(chat_id, random.choice(msg))
-            # bot.sendChatAction(chat_id, 'upload_document')
-            # bot.sendDocument(chat_id, "BQADBAADdwMAAgMdZAdPtWmOPGN1IQI")
-        elif verify_text(command.lower().split(), 'trans'):
-            msg = command.lower().replace('trans ', '')
-            if msg.split()[0] == 'pt':
-                msg = msg.replace("pt ", "", 1)
-                print msg
-                os.system('echo "{}" | trans -b -o ~/output.txt :pt'.format(msg))
+                bot.sendDocument(chat_id, "BQADAQADEwADnqxzCGp0fqkzsPC6Ag")
+            elif command.lower() == 'nós te amamos!':
+                msg = "Ah é?! Foda-se."
+                bot.sendMessage(chat_id, msg)
+                bot.sendChatAction(chat_id, 'upload_document')
+                bot.sendDocument(chat_id, "BQADBAADYwMAAiUcZAe1DjlP-IMGhQI")
+            elif command.lower() == 'é bininu binina ou binunu binino?'.lower():
+                msg = "bininu."
+                bot.sendMessage(chat_id, msg)
+            elif command.lower() == 'qual sua idade?':
+                msg = "Você sabe a idade de Deus, seu criador? Pois é, sou 1 ano mais novo que Ele."
+                bot.sendMessage(chat_id, msg)
+            elif command.lower() == '/emojis':
+                msg1, msg2, msg3, msg4, msg5 = get_all_emojis()
+                bot.sendMessage(chat_id, msg1)
+                bot.sendMessage(chat_id, msg2)
+                bot.sendMessage(chat_id, msg3)
+                bot.sendMessage(chat_id, msg4)
+                bot.sendMessage(chat_id, msg5)
+            elif command.lower() in fuck_words:
+                msg = [
+                    "Querido, por favor! Tenha boas maneiras! Você tem que me convidar pra jantar primeiro.",
+                    "Entre na fila.",
+                    "Sonhando novamente, querido?",
+                    "Se sentindo sozinho de novo, ha?",
+                    "É só eu ou você diz isso para todos?",
+                    "Sério? Agora?",
+                    "Não obrigado. Eu passo.",
+                ]
+                bot.sendMessage(chat_id, random.choice(msg))
+                # bot.sendChatAction(chat_id, 'upload_document')
+                # bot.sendDocument(chat_id, "BQADBAADdwMAAgMdZAdPtWmOPGN1IQI")
+            elif verify_text(command.lower().split(), 'trans'):
+                msg = command.lower().replace('trans ', '')
+                if msg.split()[0] == 'pt':
+                    msg = msg.replace("pt ", "", 1)
+                    print msg
+                    os.system('echo "{}" | trans -b -o ~/output.txt :pt'.format(msg))
+                else:
+                    os.system('echo "{}" | trans -b -o ~/output.txt :en'.format(msg))
+                with open(os.environ['HOME'] + '/output.txt', 'r') as content_file:
+                    content = content_file.read()
+                bot.sendMessage(chat_id, content)
+            elif command == 'cotação':
+                quotes, status = get_current_quote()
+                dolar_value, euro_value = float(quotes.get('dolar').get('cotacao')), float(quotes.get('euro').get('cotacao'))
+                dolar_var, euro_var = quotes.get('dolar').get('variacao'), quotes.get('euro').get('variacao')
+                quote_coffee = QuoteCoffee(db).get_quote()
+                updated_at = quotes.get('atualizacao')
+                msg = """
+                Dólar: R$ %.2f (%s)\nEuro: R$ %.2f (%s)\nCafé Arábica 6 Sc: R$ %.2f (%s)\nAtualizado em %s hrs
+                """ % (dolar_value, dolar_var, euro_value, euro_var,
+                       quote_coffee.get('quote_value', ''),
+                       quote_coffee.get('rate', ''),
+                       updated_at.replace('\/', '-').replace('  -', 'às'),)
+                bot.sendMessage(chat_id, re.sub(' +', ' ', msg.replace('.', ',')))
             else:
-                os.system('echo "{}" | trans -b -o ~/output.txt :en'.format(msg))
-            with open(os.environ['HOME'] + '/output.txt', 'r') as content_file:
-                content = content_file.read()
-            bot.sendMessage(chat_id, content)
-        elif command == 'cotação':
-            quotes, status = get_current_quote()
-            dolar_value, euro_value = float(quotes.get('dolar').get('cotacao')), float(quotes.get('euro').get('cotacao'))
-            dolar_var, euro_var = quotes.get('dolar').get('variacao'), quotes.get('euro').get('variacao')
-            quote_coffee = QuoteCoffee(db).get_quote()
-            updated_at = quotes.get('atualizacao')
-            msg = """
-            Dólar: R$ %.2f (%s)\nEuro: R$ %.2f (%s)\nCafé Arábica 6 Sc: R$ %.2f (%s)\nAtualizado em %s hrs
-            """ % (dolar_value, dolar_var, euro_value, euro_var,
-                   quote_coffee.get('quote_value', ''),
-                   quote_coffee.get('rate', ''),
-                   updated_at.replace('\/', '-').replace('  -', 'às'),)
-            bot.sendMessage(chat_id, re.sub(' +', ' ', msg.replace('.', ',')))
-        else:
-            cnt_ed = count_ed_mgs(db)
-            cnt_simsimi = count_simsimi_msg(db)
-            sminino_group_id = -58208727
-            on_the_music_group_id = -82861655
-            limit_ed = 5 if chat_id == sminino_group_id else 1 if chat_id == on_the_music_group_id else 3
-            limit_simsimi = 2 if chat_id == sminino_group_id else 5 if chat_id == on_the_music_group_id else 3
-            print 'limit_ed', limit_ed
-            print 'limit_simsimi', limit_simsimi
-            if cnt_ed < limit_ed:
-                response, sim_status, robot_name = get_ed_reply(command)
-            elif cnt_simsimi < limit_simsimi:
-                response, sim_status, robot_name = get_simsimi_reply(command)
-            else:
-                response, ed_status, robot_name = get_ed_reply(command)
-                if ed_status != 200:
+                cnt_ed = count_ed_mgs(db)
+                cnt_simsimi = count_simsimi_msg(db)
+                sminino_group_id = -58208727
+                on_the_music_group_id = -82861655
+                limit_ed = 5 if chat_id == sminino_group_id else 1 if chat_id == on_the_music_group_id else 3
+                limit_simsimi = 2 if chat_id == sminino_group_id else 5 if chat_id == on_the_music_group_id else 3
+                print 'limit_ed', limit_ed
+                print 'limit_simsimi', limit_simsimi
+                if cnt_ed < limit_ed:
+                    response, sim_status, robot_name = get_ed_reply(command)
+                elif cnt_simsimi < limit_simsimi:
                     response, sim_status, robot_name = get_simsimi_reply(command)
-                q = {'qty_answed_message': 0}
-                db.set('ed_info', q)
-                db.set('simsimi_info', q)
-                db.dump()
+                else:
+                    response, ed_status, robot_name = get_ed_reply(command)
+                    if ed_status != 200:
+                        response, sim_status, robot_name = get_simsimi_reply(command)
+                    q = {'qty_answed_message': 0}
+                    db.set('ed_info', q)
+                    db.set('simsimi_info', q)
+                    db.dump()
 
-            if verify_text(['Fui criado e program', 'O meu inventor'], response):
-                developed_by_texts = db.get('developed_by')
-                olds = [utf8_encode(text) for text in developed_by_texts['old']]
-                news = [utf8_encode(text) for text in developed_by_texts['new']]
-                if 'Fui criado e program' in response:
-                    response = response.replace(
-                            olds[0],
-                            news[0])
-                    response += ' {}'.format(Emoji.GRINNING_FACE)
-                if 'O meu inventor' in response:
-                    response = response.replace(
-                            olds[1],
-                            news[1])
-            info_sent = bot.sendMessage(chat_id, response)
-            if info_sent:
-                print robot_name
-                try:
-                    count_msg = db.get('{}_info'.format(robot_name))['qty_answed_message']
-                    count_msg += 1
-                except:
-                    count_msg = 0
-                q = {'qty_answed_message': count_msg}
-                db.set('{}_info'.format(robot_name), q)
-                db.dump()
-    elif verify_text(command.lower().split(), 'kkk'*15):
-            msgs = [
-                'hahaha',
-                'kkkk',
-            ]
-            bot.sendMessage(chat_id, random.choice(msgs))
+                if verify_text(['Fui criado e program', 'O meu inventor'], response):
+                    developed_by_texts = db.get('developed_by')
+                    olds = [utf8_encode(text) for text in developed_by_texts['old']]
+                    news = [utf8_encode(text) for text in developed_by_texts['new']]
+                    if 'Fui criado e program' in response:
+                        response = response.replace(
+                                olds[0],
+                                news[0])
+                        response += ' {}'.format(Emoji.GRINNING_FACE)
+                    if 'O meu inventor' in response:
+                        response = response.replace(
+                                olds[1],
+                                news[1])
+                info_sent = bot.sendMessage(chat_id, response)
+                if info_sent:
+                    print robot_name
+                    try:
+                        count_msg = db.get('{}_info'.format(robot_name))['qty_answed_message']
+                        count_msg += 1
+                    except:
+                        count_msg = 0
+                    q = {'qty_answed_message': count_msg}
+                    db.set('{}_info'.format(robot_name), q)
+                    db.dump()
+        elif verify_text(command.lower().split(), 'kkk'*15):
+                msgs = [
+                    'hahaha',
+                    'kkkk',
+                ]
+                bot.sendMessage(chat_id, random.choice(msgs))
+    except Exception as e:
+        logger.exception(e)
 
 
 bot = telepot.Bot(db.get('TOKEN'))
