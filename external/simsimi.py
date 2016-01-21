aaef9d0c9ea103f51f2c72a7b72f96eed4365a88
Alexsander Falcucci <alex.falcucci@gmail.com>
2016-01-21 12:21:42 -0200

    get new simsimi API to answer

--- external/simsimi.py
+++ external/simsimi.py
@@ -8,6 +8,7 @@ import urllib3
 import re
 from utils.utils import convert_str_to_dict
 from external.ed import get_ed_reply
+import json
 
 
 # Regular Expression to remove html tags r'<\/?\w+\s*[^>]*?\/?>'
@@ -16,8 +17,11 @@ pattern = re.compile(u'<\/?\w+\s*[^>]*?\/?>', re.DOTALL | re.MULTILINE | re.IGNO
 
 def get_simsimi_reply(user_text):
     http = urllib3.PoolManager()
-    url = ("http://www.simsimi.com/requestChat?lc=pt&ft=1.0&req=" + urllib.quote_plus(user_text) +
-           "&uid=83530944&did=0")
+    api_key = "0da76951-3f0d-49be-9be3-e014f658644b"
+    url = (
+        "http://sandbox.api.simsimi.com/request.p"
+        "?key=" + api_key + "&lc=pt&ft=1.0&text=" + urllib.quote_plus(user_text)
+    )
     r = http.request('GET', url)
     status = r.status
     name = 'simsimi'
@@ -25,12 +29,12 @@ def get_simsimi_reply(user_text):
     response_text = pattern.sub(" ", text)
     if response_text:
         try:
-            convert_str_to_dict(response_text)
-            dict_ = convert_str_to_dict(response_text)
-            reply_text = dict_.get('res').get('msg')
+            dict_ = json.loads(response_text)
+            reply_text = dict_.get('response')
         except SyntaxError:
             reply_text, status, name = get_ed_reply(user_text)
-        if 'I HAVE NO RESPONSE' in reply_text:
+        print reply_text
+        if not reply_text:
             print 'I HAVE NO RESPONSE'
             reply_text, status, name = get_ed_reply(user_text)
             print reply_text
